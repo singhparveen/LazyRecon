8f2102e1af793a3bc97792f7fbc57ffcff89b800
Parveen Singh <singh_parveen@outlook.com>
2020-06-11 21:09:41 -0700

    Modifiying amass installation command to use snap directory

--- get-go.sh
+++ get-go.sh
@@ -7,6 +7,7 @@ echo -e "${RED}[+] Installing the latest version of Go...${RESET}"
 LATEST_GO=$(wget -qO- https://golang.org/dl/ | grep -oP 'go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2)
 wget https://dl.google.com/go/go$LATEST_GO.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go$LATEST_GO.linux-amd64.tar.gz
+mkdir -p /go/bin
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 source ~/.profile
 rm -rf go$LATEST_GO*
